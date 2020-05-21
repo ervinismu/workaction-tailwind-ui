74ef0c86aa4a93bab201f5107da29982fd45ed36
Ervin Ismu <social.ervin@gmail.com>
2020-05-21 10:27:48 +0700

    Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+    customForms: theme => ({
+      default: {
+        input: {
+          borderRadius: theme('borderRadius.lg'),
+          backgroundColor: theme('colors.gray.700'),
+          borderColor: 'transparent',
+        }
+      }
+      // default: {
+      //   input: {
+      //     borderRadius: theme('borderRadius.lg'),
+      //     backgroundColor: theme('colors.gray.200'),
+      //     '&:focus': {
+      //       backgroundColor: theme('colors.white'),
+      //     }
+      //   },
+      //   select: {
+      //     borderRadius: theme('borderRadius.lg'),
+      //     boxShadow: theme('boxShadow.default'),
+      //   },
+      //   checkbox: {
+      //     width: theme('spacing.6'),
+      //     height: theme('spacing.6'),
+      //   },
+      // },
+    })
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/custom-forms'),
+  ],
+}
